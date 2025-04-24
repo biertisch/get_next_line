e43a4872ef888f5ff354939804ec0a2eb3a6b563
almei <almeidarodrigues.beatriz@gmail.com>
2025-04-24 13:36:47 +0100

    modified

--- get_next_line.c
+++ get_next_line.c
@@ -20,7 +20,7 @@ char	*get_next_line(int fd)
 	char		*line;
 	ssize_t		bytes_read;
 
-	if (fd == -1 || BUFFER_SIZE < 0)
+	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
 	line = NULL;
 	while (1)
@@ -29,93 +29,106 @@ char	*get_next_line(int fd)
 		{
 			bytes_read = read(fd, buffer, BUFFER_SIZE);
 			if (bytes_read <= 0)
+			{
+				if (bytes_read < 0)
+					clear_line(&line);
 				return (line);
+			}
 			buffer[bytes_read] = '\0';
 		}
 		extract_line(buffer, &line);
-		if (!line)
-			return (NULL);
 		reset_buffer(buffer);
-		if (line[line_len(line)] == '\n')
+		if (!line || line[line_len(line)] == '\n')
 			return (line);
 	}
 }
 
-int	main(void)
+/*int	main(void)
 {
 	char	*line;
 	int		fd;
-	int		i;
 
 	printf("TEST 0: .txt with 4 short lines, all terminating with \\n\n");
 	fd = open("data/test0.txt", O_RDONLY);
-	i = 0;
 	while ((line = get_next_line(fd)))
 	{
-		printf("line %i: %s", i, line);
+		printf("%s", line);
 		free(line);
-		i++;
 	}
 	close(fd);
 	printf("\n");
-
 	
 	printf("TEST 1: .txt with 2 short lines, the last one without \\n\n");
 	fd = open("data/test1.txt", O_RDONLY);
-	i = 0;
 	while ((line = get_next_line(fd)))
 	{
-		printf("line %i: %s", i, line);
+		printf("%s", line);
 		free(line);
-		i++;
 	}
 	close(fd);
 	printf("\n");
 
 	printf("TEST 2: .txt with 2 long lines, both terminating with \\n\n");
 	fd = open("data/test2.txt", O_RDONLY);
-	i = 0;
 	while ((line = get_next_line(fd)))
 	{
-		printf("line %i: %s", i, line);
+		printf("%s", line);
 		free(line);
-		i++;
 	}
 	close(fd);
 	printf("\n");
 
 	printf("TEST 3: empty .txt\n");
 	fd = open("data/test3.txt", O_RDONLY);
-	i = 0;
 	while ((line = get_next_line(fd)))
 	{
-		printf("line %i: %s", i, line);
+		printf("%s", line);
 		free(line);
-		i++;
 	}
 	close(fd);
 	printf("\n");
 
-	return (0);
-}
+	printf("TEST 4: standard input\n");
+	line = get_next_line(0);
+	printf("%s", line);
+	free(line);
+	printf("\n");
+
+	printf("TEST 5: .c with 6 lines\n");
+	fd = open("data/test5.c", O_RDONLY);
+	while ((line = get_next_line(fd)))
+	{
+		printf("%s", line);
+		free(line);
+	}
+	close(fd);
+	printf("\n");
+
+	printf("TEST 6: directory\n");
+	fd = open("data", O_RDONLY);
+	while ((line = get_next_line(fd)))
+	{
+		printf("%s", line);
+		free(line);
+	}
+	close(fd);
+	printf("\n");
+
+	printf("TEST 7: fd == -1\n");
+	while ((line = get_next_line(-1)))
+	{
+		printf("%s", line);
+		free(line);
+	}
+	printf("\n");
+}*/
 
 /*TEST
-1. fd = standard input(0)
-2. fd= .c file
-3. fd = .out file
-4. fd = .jpg ??
-5. fd = directory
-6. fd = pipes, sockets, terminals ??
-7. fd = -1 (fail doesn't exist, permission denied)
-
-10. loop until EOF
-11. call function once
-12. call function twice
-
-13. buffer_size = 42
-14. buffer_size = 1
-15. buffer_size = 9999
-16. without D flag
-17. buffer_size = 0
-18. buffer_size = -1
+BUFFER_SIZE = 42
+BUFFER_SIZE = 1
+BUFFER_SIZE = 10
+BUFFER_SIZE = 9999
+BUFFER_SIZE = 0
+BUFFER_SIZE = -1
+without -D flag
 */
